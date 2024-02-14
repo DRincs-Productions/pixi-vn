d663e8eb33b3566e0a1da0e9ad944679aaafcd32
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-02-14 16:07:53 +0100

    pixijs exemple

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Sprite, Stage, useTick } from '@pixi/react';
+import { useReducer, useRef } from 'react';
+
+type reducertype = {
+    type: string,
+    data: any
+}
+
+const Bunny = () => {
+    const reducer = (a: any, data: reducertype) => data.data;
+    const [motion, update] = useReducer(reducer, undefined);
+    const iter = useRef(0);
+
+    useTick((delta) => {
+        const i = (iter.current += 0.05 * delta);
+
+        update({
+            type: 'update',
+            data: {
+                x: Math.sin(i) * 100,
+                y: Math.sin(i / 1.5) * 100,
+                rotation: Math.sin(i) * Math.PI,
+                anchor: Math.sin(i / 2),
+            },
+        });
+    });
+    try {
+        return <Sprite image="https://pixijs.io/pixi-react/img/bunny.png" {...motion} />;
+    }
+    catch (e) {
+        console.log(e);
+    }
+    return <></>;
+};
 
 function App() {
-return (
-<div className="App">
-<header className="App-header">
-<img src={logo} className="App-logo" alt="logo" />
-<p>
-Edit <code>src/App.tsx</code> and save to reload.
-</p>
-<a
-className="App-link"
-href="https://reactjs.org"
-target="_blank"
-rel="noopener noreferrer"
->
-Learn React
-</a>
-</header>
-</div>
-);
+    return (
+        <Stage width={300} height={300} options={{ backgroundAlpha: 0 }}>
+            <Container x={150} y={150}>
+                <Bunny />
+            </Container>
+        </Stage>
+    );
 }
 
 export default App;
