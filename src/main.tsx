90e93aa5915d83c908a6f0328e0c3a36b70ad520
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-02-15 15:58:07 +0100

    add pixi js and react

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as PIXI from 'pixi.js';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+// Canvas setup with PIXI
+const canvas = document.body
+if (!canvas) {
+    throw new Error('body element not found');
+}
+
+const app = new PIXI.Application({
+    background: '#1099bb',
+    resizeTo: window,
+});
+
+canvas.appendChild(app.view as HTMLCanvasElement);
+
+// React setup with ReactDOM
+const reactRoot = document.getElementById('root')
+if (!reactRoot) {
+    throw new Error('root element not found');
+}
+
+ReactDOM.createRoot(reactRoot).render(
     <App />
-  </React.StrictMode>,
 )
