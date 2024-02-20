e9cf6947bf79138871dd7c17c42c1cdb055d5442
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-02-20 17:03:32 +0100

    MainMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import MainMenu from './menu/MainMenu';
+
 function App() {
 
     return (
         <>
+            <Routes>
+                <Route key={"main_menu"} path={"/"} element={<MainMenu />} />
+            </Routes>
         </>
     )
 }
