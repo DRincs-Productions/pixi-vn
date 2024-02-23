392512b563548607e29d68667d4cb859779b9191
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-02-23 17:02:34 +0100

    HistoryManager

--- src/lib/HistoryManager.ts
+++ src/lib/HistoryManager.ts
@@ -1,15 +1,56 @@
 import { Label } from "./Label"
-import { Step } from "./Step"
 
-type HystoryElement = {
+type StepHistory = string
+
+enum LabelEventEnum {
+    End = "end",
+    OpenByCall = "openbycall",
+    OpenByJump = "openbyjump",
+}
+
+interface HistoryLabel {
+    type: LabelEventEnum,
+    label: Label,
+}
+
+interface HistoryStep {
     path: string,
     storage: object,
-    lastLabet: Label,
-    lastStep: Step,
-    lastStepNumber: number,
+    currentStep: StepHistory,
+    currentStepNumber: number,
 }
+
+type HystoryElement = HistoryLabel | HistoryStep
 
 export class HistoryManager {
     private constructor() { }
-    public history: HystoryElement[] = []
+    public static stepHistory: HystoryElement[] = []
+    public static lastLabel: Label | null = null
+    public static next() {
+        if (HistoryManager.lastLabel) {
+            // TODO: add check if is development and if is a laod save
+            let steps = HistoryManager.stepsAfterLastHistoryLabel
+            let res = HistoryManager.lastLabel.findLastStepIndex(steps)
+            // TODO: remove the inutil steps
+            // TODO: load lastest step
+
+            // TODO: else get the next step by index
+        }
+    }
+    public static get stepsAfterLastHistoryLabel(): string[] {
+        let length = HistoryManager.stepHistory.length
+        let steps: string[] = []
+        for (let i = length - 1; i >= 0; i--) {
+            let element = HistoryManager.stepHistory[i]
+            if (typeof element === "object" && "currentStep" in element) {
+                steps.push(element.currentStep)
+            }
+            else {
+                break
+            }
+        }
+
+        steps = steps.reverse()
+        return steps
+    }
 }
--- src/lib/Label.ts
+++ src/lib/Label.ts
@@ -1,6 +1,34 @@
+export type StepLabel = (() => void | Promise<void>)
+type StepEquivelent = {
+    hystoryIndex: number,
+    labelIndex: number,
+}
+
 export abstract class Label {
-    abstract getSteps(): (string);
+    abstract getSteps(): StepLabel[];
     public get steps() {
         return this.getSteps()
+    }
+    public checkIfStepIsEqual(step: string, step2: StepLabel): boolean {
+        return step === step2.toString()
+    }
+    public findLastStepIndex(hystorySteps: string[]): StepEquivelent | null {
+        let hypotheticalIndex: number = hystorySteps.length - 1
+        if (hystorySteps.length === 0) {
+            return null
+        }
+        if (hystorySteps.length === 1) {
+            let step = hystorySteps[0]
+            if (this.checkIfStepIsEqual(step, this.steps[0])) {
+                return { hystoryIndex: 0, labelIndex: 0 }
+            }
+            else {
+                return null
+            }
+        }
+        // TODO: find the bests combination of steps
+        // TODO: Knapsack Problem
+        // TODO: return the best combination near the hypotheticalIndex
+        return null
     }
 }
