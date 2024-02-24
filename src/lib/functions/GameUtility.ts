907519c790d54ca32245805f49c1c0313d657bae
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-02-24 11:20:46 +0100

    Add clearAllGameDatas function to reset the game

--- src/lib/functions/GameUtility.ts
+++ src/lib/functions/GameUtility.ts
@@ -2,6 +2,9 @@ import { GameHistoryManager } from "../managers/HistoryManager";
 import { GameStorageManager } from "../managers/StorageManager";
 import { GameWindowManager } from "../managers/WindowManager";
 
+/**
+ * Clear all game data. This function is used to reset the game.
+ */
 export function clearAllGameDatas() {
     GameStorageManager.clear();
     GameWindowManager.removeChildren();
