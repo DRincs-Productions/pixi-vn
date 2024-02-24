1abb3a9d1046a72752c51524b01dceab7b66ff78
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-02-24 09:49:04 +0100

    Add comments and improve code readability in Label.ts

--- src/lib/Label.ts
+++ src/lib/Label.ts
@@ -1,8 +1,19 @@
 export type StepLabel = (() => void | Promise<void>)
 export type StepHistoryData = string
+/**
+ * Convert StepLabel to StepHistoryData
+ * @param step
+ * @returns
+ */
 export function convertStelLabelToStepHistoryData(step: StepLabel): StepHistoryData {
     return step.toString().toLocaleLowerCase()
 }
+/**
+ * Check if two steps are equal
+ * @param step1
+ * @param step2
+ * @returns
+ */
 export function checkIfStepsIsEqual(step1: StepHistoryData | StepLabel, step2: StepHistoryData | StepLabel): boolean {
     if (typeof step1 === "function") {
         step1 = convertStelLabelToStepHistoryData(step1)
@@ -13,17 +24,26 @@ export function checkIfStepsIsEqual(step1: StepHistoryData | StepLabel, step2: S
     return step1.toLocaleLowerCase() === step2.toLocaleLowerCase()
 }
 
+/**
+ * Label is a class that contains a list of steps, which will be performed as the game continues.
+ */
 export abstract class Label {
-    abstract getSteps(): StepLabel[];
-    public get steps() {
-        return this.getSteps()
-    }
-    public getCorrespondingStepsNumber(steps: StepHistoryData[]): number {
-        if (steps.length === 0) {
+    /**
+     * Get the steps of the label.
+     * Every time you update this list will also be updated when the other game versions load.
+     */
+    public abstract get steps(): StepLabel[]
+    /**
+     * Get the corresponding steps number
+     * @param externalSteps
+     * @returns Numer of corresponding steps, for example, if externalSteps is [ABC, DEF, GHI] and the steps of the label is [ABC, GHT], the result will be 1
+     */
+    public getCorrespondingStepsNumber(externalSteps: StepHistoryData[] | StepLabel[]): number {
+        if (externalSteps.length === 0) {
             return 0
         }
         let res: number = 0
-        steps.forEach((step, index) => {
+        externalSteps.forEach((step, index) => {
             if (checkIfStepsIsEqual(step, this.steps[index])) {
                 res = index
             }
