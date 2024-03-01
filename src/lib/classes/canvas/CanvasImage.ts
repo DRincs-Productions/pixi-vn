990acdc753d9b8e3dac47c91df623fe0203d6424
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-03-01 09:57:14 +0100

    Update CanvasImage class to use Sprite instead of Container

--- src/lib/classes/canvas/CanvasImage.ts
+++ src/lib/classes/canvas/CanvasImage.ts
@@ -1,24 +1,26 @@
-import { Container } from "pixi.js";
+import { Sprite } from "pixi.js";
 import { getImageSprite, getImageSpriteAsync } from "../../functions/ImageUtility";
 import { ICanvasImageMemory } from "../../interface/canvas/ICanvasImageMemory";
 import { CanvasContainerBase } from "./CanvasContainer";
+import { CanvasSprite } from "./CanvasSprite";
 
 /**
  * The base class for the image.
  */
-abstract class CanvasImageBase extends CanvasContainerBase<Container, ICanvasImageMemory> {
+abstract class CanvasImageBase extends CanvasContainerBase<Sprite, ICanvasImageMemory> {
     imageLink: string
     constructor(image: string) {
-        let container = new Container()
-        super(container)
+        let element = new Sprite() // TODO add loader animation
+        super(element)
         this.imageLink = image
         this.updateImage(image)
     }
     /**
      * Update the image of the element.
      * @param image is the url of the image.
+     * @returns the sprite of the image.
      */
-    abstract updateImage(image: string): void
+    abstract updateImage(image: string): CanvasSprite | Promise<CanvasSprite>
     get memory(): ICanvasImageMemory {
         return {
             ...super.memoryContainer,
@@ -39,10 +41,11 @@ export class CanvasImage extends CanvasImageBase {
     get className(): string {
         return this.constructor.name
     }
-    updateImage(image: string) {
+    updateImage(image: string): CanvasSprite {
         this.imageLink = image
         let element = getImageSprite(image)
         this.addChild(element)
+        return element
     }
 }
 
@@ -53,14 +56,16 @@ export class CanvasImageAsync extends CanvasImageBase {
     get className(): string {
         return this.constructor.name
     }
-    updateImage(image: string): void {
+    async updateImage(image: string): Promise<CanvasSprite> {
         this.imageLink = image
-        getImageSpriteAsync(image)
+        return getImageSpriteAsync(image)
             .then((element) => {
                 this.addChild(element)
+                return element
             })
             .catch(() => {
                 console.error("Error loading image")
+                return new CanvasSprite()
             })
     }
 }
