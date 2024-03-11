650a787ed7dee8fb5aee5e3110a6a4102a911edd
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-03-11 12:30:23 +0100

    Add setMemoryContainer function to CanvasContainer

--- src/lib/classes/canvas/CanvasContainer.ts
+++ src/lib/classes/canvas/CanvasContainer.ts
@@ -11,6 +11,9 @@ export class CanvasContainer extends Container implements ICanvasBase<ICanvasCon
     get memory(): ICanvasContainerMemory {
         return getMemoryContainer(this)
     }
+    set memory(value: ICanvasContainerMemory) {
+        setMemoryContainer(this, value)
+    }
     addCanvasChild<U extends SupportedCanvasElement[]>(...children: U): U[0] {
         return super.addChild(...children)
     }
@@ -51,4 +54,40 @@ export function getMemoryContainer<T extends Container>(element: T): ICanvasCont
         y: element.y,
         boundsArea: element.boundsArea,
     }
+}
+
+export function setMemoryContainer<T extends Container>(element: T | Container, memory: ICanvasContainerMemory) {
+    memory.width && (element.width = memory.width)
+    memory.height && (element.height = memory.height)
+
+    memory.isRenderGroup && (element.isRenderGroup = memory.isRenderGroup)
+    memory.blendMode && (element.blendMode = memory.blendMode)
+    memory.tint && (element.tint = memory.tint)
+    memory.alpha && (element.alpha = memory.alpha)
+    memory.angle && (element.angle = memory.angle)
+    memory.renderable && (element.renderable = memory.renderable)
+    memory.rotation && (element.rotation = memory.rotation)
+    if (memory.scale) {
+        if (typeof memory.scale === "number") {
+            element.scale.set(memory.scale, memory.scale)
+        }
+        else {
+            element.scale.set(memory.scale.x, memory.scale.y)
+        }
+    }
+    if (memory.pivot) {
+        if (typeof memory.pivot === "number") {
+            element.pivot.set(memory.pivot, memory.pivot)
+        }
+        else {
+            element.pivot.set(memory.pivot.x, memory.pivot.y)
+        }
+    }
+    memory.position && (element.position.set(memory.position.x, memory.position.y))
+    memory.skew && (element.skew.set(memory.skew.x, memory.skew.y))
+    memory.visible && (element.visible = memory.visible)
+    memory.culled && (element.culled = memory.culled)
+    memory.x && (element.x = memory.x)
+    memory.y && (element.y = memory.y)
+    memory.boundsArea && (element.boundsArea = memory.boundsArea)
 }
