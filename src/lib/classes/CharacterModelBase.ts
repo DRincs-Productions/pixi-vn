25f0155a563f842c8e71bc3ca05989bd81b98fab
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-03-15 12:06:14 +0100

    Update function to handle edge case

---
+++ src/lib/classes/CharacterModelBase.ts
@@ -0,0 +1,43 @@
+import { StoredClassModel } from "./StoredClassModel"
+
+interface ICharacterModelBase {
+    name: string
+    surname?: string
+    age?: number
+    icon?: string
+    color?: string
+}
+
+export class CharacterModelBase extends StoredClassModel implements ICharacterModelBase {
+    constructor(id: string, props: ICharacterModelBase) {
+        super(id)
+        this.defaultName = props.name
+        this.defaultSurname = props.surname
+        this.defaultAge = props.age
+        this.icon = props.icon
+        this.color = props.color
+    }
+    private defaultName: string = ""
+    get name(): string {
+        return this.getStorageProperty<string>("name") || this.defaultName
+    }
+    set name(value: string) {
+        this.updateStorage({ ...this, name: value })
+    }
+    private defaultSurname?: string
+    get surname(): string | undefined {
+        return this.getStorageProperty<string>("surname") || this.defaultSurname
+    }
+    set surname(value: string | undefined) {
+        this.updateStorage({ ...this, surname: value })
+    }
+    private defaultAge?: number | undefined
+    get age(): number | undefined {
+        return this.getStorageProperty<number>("age") || this.defaultAge
+    }
+    set age(value: number | undefined) {
+        this.updateStorage({ ...this, age: value })
+    }
+    icon?: string | undefined
+    color?: string | undefined
+}
