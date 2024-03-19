d82e215fac5680d7b3580f9fcee1da4a641621fb
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-03-19 15:24:20 +0100

    Add new exports

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -3,3 +3,7 @@ export * from './classes/canvas';
 export * from './classes/ticker';
 export * from './decorators';
 export * from './functions';
+export * from './interface';
+export * from './interface/canvas';
+export * from './interface/export';
+
