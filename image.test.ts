1d1275b8089cdad80ba4a0cfc1ca6aa4af370536
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-03-20 16:39:41 +0100

    vitest

---
+++ image.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest';
+
+test('adds two numbers correctly', () => {
+    const result = 3 + 2
+    expect(result).toBe(5);
+});
---
+++ initial.test.ts
@@ -0,0 +1,30 @@
+import { test } from 'vitest';
+import { GameWindowManager } from './src';
+
+test('aaaa', async () => {
+    try {
+        // Canvas setup with PIXI
+        const body = document.body
+        if (!body) {
+            throw new Error('body element not found')
+        }
+
+        await GameWindowManager.initialize(body, 1920, 1080, {
+            backgroundColor: "#303030"
+        })
+
+        // React setup with ReactDOM
+        const root = document.getElementById('root')
+        if (!root) {
+            throw new Error('root element not found')
+        }
+
+        GameWindowManager.initializeHTMLLayout(root)
+        let res = GameWindowManager.isInitialized || GameWindowManager.htmlLayout
+        expect(res).toBe(true)
+    }
+    catch (e) {
+        console.error(e)
+        expect(false).toBe(true)
+    }
+});
