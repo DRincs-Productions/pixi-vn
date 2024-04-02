3f1240dd77686784affc36481f80b5b5bcd821f1
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-02 14:07:43 +0200

    Refactor IHistoryStep interface

--- src/interface/IHistoryStep.ts
+++ src/interface/IHistoryStep.ts
@@ -4,17 +4,31 @@ import ExportedStorage from "./export/ExportedStorage";
 import IOpenedLabel from "./IOpenedLabel";
 
 /**
- * IHistoryStep is a interface that contains:
- * - the browser path that occurred during the progression of the steps.
- * - the storage that occurred during the progression of the steps.
- * - the step data.
- * - the canvas that occurred during the progression of the steps.
+ * IHistoryStep is a interface that contains the information of a step in the history.
  */
 export default interface IHistoryStep {
+    /**
+     * The browser path that occurred during the progression of the steps.
+     */
     path: string,
+    /**
+     * The storage that occurred during the progression of the steps.
+     */
     storage: ExportedStorage,
+    /**
+     * The sha1 of the step function.
+     */
     stepSha1: StepHistoryDataType,
+    /**
+     * The index of the step.
+     */
     stepIndex: number,
+    /**
+     * The canvas that occurred during the progression of the steps.
+     */
     canvas: ExportedCanvas,
+    /**
+     * The opened labels that occurred during the progression of the steps.
+     */
     openedLabels: IOpenedLabel[],
 }
