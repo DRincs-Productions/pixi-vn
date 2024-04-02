5960762d8f04aca250872b14b22b02e2e2547d1d
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-02 10:04:59 +0200

    Update showImageWithDisolveEffect to showImageWithDissolveTransition

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,6 +1,6 @@
 export { clearDialogue, clearMenuOptions, getDialogue, getDialogueHistory, getMenuOptions, setDialogue, setMenuOptions } from './DialogueUtility';
 export { clearAllGameDatas } from './GameUtility';
-export { addImage, removeCanvasElement, showCanvasImages, showImageWithDisolveEffect } from './ImageUtility';
+export { addImage, removeCanvasElement, showCanvasImages, showImageWithDissolveTransition } from './ImageUtility';
 export { getSaveData, getSaveJson, loadSaveData, loadSaveJson } from './SavesUtility';
 export { getTexture } from './TextureUtility';
 
