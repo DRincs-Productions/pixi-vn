f2b645f5108f49c3f252a6b4fb33d83aee04e37c
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-02 14:16:36 +0200

    Add ChoiceMenuOptionLabel interface

--- src/classes/ChoiceMenuOptionLabel.ts
+++ src/classes/ChoiceMenuOptionLabel.ts
@@ -9,12 +9,27 @@ import Label from "./Label"
  * ```
  */
 export default class ChoiceMenuOptionLabel {
+    /**
+     * Text to be displayed in the menu
+     */
     text: string
+    /**
+     * Label to be opened when the option is selected
+     */
     label: typeof Label
+    /**
+     * Type of the label to be opened
+     */
     type: LabelRunModeEnum
     constructor(text: string, label: typeof Label, type: LabelRunModeEnum = LabelRunModeEnum.OpenByCall) {
         this.text = text
         this.label = label
         this.type = type
     }
+}
+
+export interface IStoratedChoiceMenuOptionLabel {
+    text: string
+    label: string
+    type: LabelRunModeEnum
 }
