64b919168cbf52307a66886e5977bf9fdfa5e2ae
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-21 22:03:29 +0200

    Refactor setFlag and getFlag functions in FlagsUtility.ts

--- src/functions/FlagsUtility.ts
+++ src/functions/FlagsUtility.ts
@@ -1,13 +1,31 @@
 import { GameStorageManager } from "../managers"
 
+/**
+ * Set a flag to true or false. 
+ * @param name The name of the flag
+ * @param value The value of the flag.
+ */
 export function setFlag(name: string, value: boolean) {
-    GameStorageManager.setVariable(GameStorageManager.keysSystem.FLAGS_CATEGORY_KEY + name, value)
+    let flags = GameStorageManager.getVariable<string[]>(GameStorageManager.keysSystem.FLAGS_CATEGORY_KEY) || []
+    if (value) {
+        if (!flags.includes(name)) {
+            flags.push(name)
+        }
+    } else {
+        let index = flags.indexOf(name)
+        if (index > -1) {
+            flags.splice(index, 1)
+        }
+    }
+    GameStorageManager.setVariable(GameStorageManager.keysSystem.FLAGS_CATEGORY_KEY, flags)
 }
 
-export function getFlag(name: string): boolean | undefined {
-    return GameStorageManager.getVariable<boolean>(GameStorageManager.keysSystem.FLAGS_CATEGORY_KEY + name)
-}
-
-export function removeFlag(name: string) {
-    GameStorageManager.removeVariable(GameStorageManager.keysSystem.FLAGS_CATEGORY_KEY + name)
+/**
+ * Get the value of a flag
+ * @param name The name of the flag
+ * @returns The value of the flag
+ */
+export function getFlag(name: string): boolean {
+    let flags = GameStorageManager.getVariable<string[]>(GameStorageManager.keysSystem.FLAGS_CATEGORY_KEY) || []
+    return flags.includes(name)
 }
