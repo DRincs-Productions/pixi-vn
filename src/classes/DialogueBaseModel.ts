64039348da09f5e90511b53c28a3fda55d2bf0f4
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-02 11:32:34 +0200

    Refactor DialogueBaseModel constructor to accept character object instead of characterId

--- src/classes/DialogueBaseModel.ts
+++ src/classes/DialogueBaseModel.ts
@@ -1,10 +1,17 @@
+import CharacterBaseModel from "./CharacterBaseModel"
+
 /**
  * Base class for all dialogue models. I suggest you extend this class to create your own dialogue models.
  */
 export default class DialogueBaseModel {
-    constructor(text: string, characterId: string | undefined) {
+    constructor(text: string, character: string | CharacterBaseModel | undefined) {
         this.text = text
-        this.characterId = characterId
+        if (typeof character === "string") {
+            this.characterId = character
+        }
+        else {
+            this.characterId = character?.id
+        }
     }
     /**
      * The text of the dialogue.
