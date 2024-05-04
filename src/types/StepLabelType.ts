e51296a121037e97db265bd8b6d11e501ab325d3
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-04 11:01:32 +0200

    refactor: Update StepManager and StepLabelType to return StepLabelResultType

--- src/types/StepLabelType.ts
+++ src/types/StepLabelType.ts
@@ -1,4 +1,16 @@
 /**
  * StepLabel is a function that will be executed as the game continues.
  */
-export type StepLabelType = (() => void | Promise<void>)
+export type StepLabelType = (() => StepLabelResultType | Promise<StepLabelResultType>)
+
+/**
+ * StepLabelResultType is the return type of the StepLabel function.
+ * It can be useful for returning to the information calling function to perform other operations that cannot be performed within the StepLabel.
+ */
+export type StepLabelResultType = {
+    /**
+     * The new route to navigate to.
+     */
+    newRoute?: string,
+    [key: string]: any
+} | void
