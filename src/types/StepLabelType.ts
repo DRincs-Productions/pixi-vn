dbab90daa32c1ea79ad2ab9b302c1b74eae84a78
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-16 10:19:50 +0200

    refactor: Update StepLabelType to accept props parameter

--- src/types/StepLabelType.ts
+++ src/types/StepLabelType.ts
@@ -1,16 +1,17 @@
 /**
- * StepLabel is a function that will be executed as the game continues.
+ * StepLabelPropsType is the type of the props that will be passed to the StepLabel.
  */
-export type StepLabelType = (() => StepLabelResultType | Promise<StepLabelResultType>)
+export type StepLabelPropsType = {
+    /**
+     * the function that will be executed for navigate to another route.
+     * @param route 
+     * @returns 
+     */
+    navigateTo?: (route: string) => void,
+    [key: string]: any
+}
 
 /**
- * StepLabelResultType is the return type of the StepLabel function.
- * It can be useful for returning to the information calling function to perform other operations that cannot be performed within the StepLabel.
+ * StepLabel is a function that will be executed as the game continues.
  */
-export type StepLabelResultType = {
-    /**
-     * The new route to navigate to.
-     */
-    newRoute?: string,
-    [key: string]: any
-} | void
+export type StepLabelType = ((props: StepLabelPropsType) => any | Promise<any>)
