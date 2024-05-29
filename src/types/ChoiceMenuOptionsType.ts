0bf73ede4b6d951fa6d65baca3866cc6ccb0f92a
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-29 14:56:08 +0200

    refactor: Update ChoiceMenuOptionsType to support generic type parameter

--- src/types/ChoiceMenuOptionsType.ts
+++ src/types/ChoiceMenuOptionsType.ts
@@ -5,4 +5,4 @@ import { ChoiceMenuOptionClose } from "../classes/ChoiceMenuOption";
  * Munu is a type that contains a list of Label that a player can choose from.
  * For Ren'py this is the equivalent of a menu.
  */
-export type ChoiceMenuOptionsType = (ChoiceMenuOption<any> | ChoiceMenuOptionClose)[]
+export type ChoiceMenuOptionsType<T extends {} = {}> = (ChoiceMenuOption<T> | ChoiceMenuOptionClose)[]
