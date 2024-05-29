73f183d6d2a2815581e9f1306c76510544de31c9
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-29 13:44:04 +0200

    refactor: Update StepLabelType exports to include additional types

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,6 @@ export { Close, CloseType } from './CloseType';
 export { default as LabelRunModeType } from './LabelRunModeType';
 export { Pause, PauseType, PauseValueType } from './PauseType';
 export { Repeat, RepeatType } from './RepeatType';
-export { StepLabelType } from './StepLabelType';
+export { StepLabelPropsType, StepLabelResultType, StepLabelType } from './StepLabelType';
 export { StorageElementType, StorageObjectType } from './StorageElementType';
 
