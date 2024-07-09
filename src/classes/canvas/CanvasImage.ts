cbd6022efdcb3cfa494f5e67f682179bc9348bf8
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-07-09 12:17:01 +0200

    rename loadImages to loadImage

--- src/classes/canvas/CanvasImage.ts
+++ src/classes/canvas/CanvasImage.ts
@@ -1,4 +1,5 @@
 import { Sprite, SpriteOptions, Texture, TextureSourceLike } from "pixi.js";
+import { addImage, loadImage, showWithDissolveTransition } from "../../functions";
 import { getTexture } from "../../functions/TextureUtility";
 import ICanvasImageMemory from "../../interface/canvas/ICanvasImageMemory";
 import CanvasSprite, { getMemorySprite, setMemorySprite } from "./CanvasSprite";
@@ -7,7 +8,7 @@ import CanvasSprite, { getMemorySprite, setMemorySprite } from "./CanvasSprite";
  * This class is a extension of the CanvasSprite class, it has the same properties and methods,
  * but it has some features that make texture management easier.
  * You need to use CanvasImage.load() to show the image in the canvas.
- * This class is used for functions like addImage, loadImages and showWithDissolveTransition.
+ * This class is used for functions like {@link addImage}, {@link loadImage} and {@link showWithDissolveTransition}.
  * @example
  * ```typescript
  * let alien = new CanvasImage({
