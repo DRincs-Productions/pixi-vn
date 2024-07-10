4981a55bde8a65de06f411556eafbb0622bd777d
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-07-10 11:42:38 +0200

    fix

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 export type { default as CanvasEventNamesType } from './CanvasEventNamesType';
 export type { ChoiceMenuOptionsType } from './ChoiceMenuOptionsType';
-export type { Close, CloseType } from './CloseType';
+export { Close } from './CloseType';
+export type { CloseType } from './CloseType';
 export type { default as LabelRunModeType } from './LabelRunModeType';
 export type { Pause, PauseType, PauseValueType } from './PauseType';
 export type { Repeat, RepeatType } from './RepeatType';
