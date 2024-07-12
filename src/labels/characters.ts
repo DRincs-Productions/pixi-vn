80dde575507e04ab6adc92e76e608d800c743695
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-07-12 18:56:11 +0200

    improve

--- src/labels/characters.ts
+++ src/labels/characters.ts
@@ -4,7 +4,7 @@ import { saveCharacter } from "../decorators";
 export const juliette = new CharacterBaseModel('___pixivn_juliette___', {
     name: 'Juliette',
     age: 25,
-    icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4eMoz7DH8l_Q-iCzSc1xyu_C2iryWh2O9_FcDBpY04w&s",
+    icon: "https://firebasestorage.googleapis.com/v0/b/pixi-vn.appspot.com/o/public%2Fcharacters%2Fjuliette-square.webp?alt=media&token=5856ae7b-d99e-4563-86ec-cbc48cc9c6b4",
     color: "#ac0086"
 });
 
