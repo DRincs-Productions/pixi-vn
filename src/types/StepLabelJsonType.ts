7a0c9326aa10dcff303c0c6c06cb1f63350b5ff6
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-09 22:20:58 +0200

    add more info

--- src/types/StepLabelJsonType.ts
+++ src/types/StepLabelJsonType.ts
@@ -5,20 +5,58 @@ import { LabelIdType } from "./LabelIdType"
  * Step label JSON type
  */
 type StepLabelJsonType = {
+    /**
+     * Variable used to display a choice menu.
+     */
     currentChoiceMenuOptions?: {
+        /**
+         * The text to be displayed.
+         */
         text: string
+        /**
+         * The label id to be opened.
+         */
         label: LabelIdType
+        /**
+         * Label opening mode
+         */
         type: LabelRunModeType
+        /**
+         * The properties to be passed to the label.
+         */
         props: StorageObjectType
     }[]
+    /**
+     * Variable used to display a dialog.
+     */
     dialog?: {
+        /**
+         * The character id that will speak.
+         */
         character: string,
+        /**
+         * The text to be displayed.
+         */
         text: string,
     } | string
+    /**
+     * Variable used to open a label.
+     */
     labelToOpen?: {
+        /**
+         * The id of the label to open.
+         */
         labelId: string,
-        type: "jump" | "call",
+        /**
+         * Label opening mode
+         */
+        type: LabelRunModeType,
     }
+    /**
+     * Variable used to end some elements of the narrative.
+     * - game_end: ends the game
+     * - label_end: ends the label
+     */
     end?: "game_end" | "label_end"
 }
 
