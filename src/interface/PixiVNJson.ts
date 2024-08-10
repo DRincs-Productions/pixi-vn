10c6f663e4decf2ee984a27e90e14689cf699b9f
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-10 22:58:20 +0200

    add more info

--- src/interface/PixiVNJson.ts
+++ src/interface/PixiVNJson.ts
@@ -1,5 +1,8 @@
 import { LabelJsonType } from "../types";
 
+/**
+ * PixiVNJson It can be defined as a programming language to write a narrative written in json.
+ */
 export default interface PixiVNJson {
     labels: LabelJsonType
 }
