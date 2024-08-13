4ef47f11c1b9107d197bd4a6e76d17a7a8f908c9
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-13 16:36:52 +0200

    VideoUtility

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,6 +1,7 @@
 export { moveIn, moveOut, removeWithDissolveTransition, removeWithFadeTransition, showWithDissolveTransition, showWithFadeTransition, zoomIn, zoomOut } from './canvas/CanvasTransition';
 export { removeCanvasElement } from './canvas/CanvasUtility';
 export { addImage, loadImage, showImage } from './canvas/ImageUtility';
+export { addVideo, loadVideo, showVideo } from './canvas/VideoUtility';
 export { clearChoiceMenuOptions, clearDialogue, getChoiceMenuOptions, getDialogue, getDialogueHistory, setChoiceMenuOptions, setDialogue } from './DialogueUtility';
 export { getFlag, setFlag } from './FlagsUtility';
 export { clearAllGameDatas } from './GameUtility';
