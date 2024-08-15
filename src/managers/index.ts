23f52ecb44387da904e621c2acde65d7433b4e63
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-15 11:17:00 +0200

    improve

--- src/managers/index.ts
+++ src/managers/index.ts
@@ -1,4 +1,22 @@
-export { default as GameStepManager } from './StepManager';
-export { default as GameStorageManager } from './StorageManager';
-export { default as GameWindowManager } from './WindowManager';
+export {
+    /**
+     *  @deprecated use "import { canvas } from '@drincs/pixi-vn';"
+     */
+    default as GameStepManager,
+    default as narration
+} from './StepManager';
+export {
+    /**
+     *  @deprecated use "import { storage } from '@drincs/pixi-vn';"
+     */
+    default as GameStorageManager,
+    default as storage
+} from './StorageManager';
+export {
+    /**
+     *  @deprecated use "import { narration } from '@drincs/pixi-vn';"
+     */
+    default as GameWindowManager,
+    default as canvas
+} from './WindowManager';
 
