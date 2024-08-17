ab5a8207e839d3d92d32dffbf89567ae27e0848f
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-17 14:53:33 +0200

    SoundAudio

---
+++ src/classes/SoundAudio.ts
@@ -0,0 +1,58 @@
+import { CompleteCallback, Filter, filters, IMediaInstance, Options, PlayOptions, Sound } from "@pixi/sound";
+import { narration } from "../managers";
+import SoundManagerStatic from "../managers/SoundManagerStatic";
+
+export default class SoundAudio extends Sound {
+    alias?: string;
+    override pause(): this {
+        if (!this.alias) {
+            throw new Error("[Pixi'VN] The alias is not defined.");
+        }
+        delete SoundManagerStatic.playrdInstoSteps[this.alias];
+        return super.pause();
+    }
+    override resume(): this {
+        if (!this.alias) {
+            throw new Error("[Pixi'VN] The alias is not defined.");
+        }
+        SoundManagerStatic.playrdInstoSteps[this.alias] = {
+            step: narration.lastStepIndex
+        }
+        return super.resume();
+    }
+    override set filters(f: Filter[]) {
+        f = f.filter(f => {
+            return !(f instanceof filters.Filter);
+        })
+        super.filters = f;
+    }
+    override destroy(): void {
+        if (this.alias) {
+            delete SoundManagerStatic.playrdInstoSteps[this.alias];
+        }
+        return super.destroy();
+    }
+    override stop(): this {
+        if (!this.alias) {
+            throw new Error("[Pixi'VN] The alias is not defined.");
+        }
+        delete SoundManagerStatic.playrdInstoSteps[this.alias];
+        return super.stop();
+    }
+    override play(alias: string, callback?: CompleteCallback): IMediaInstance | Promise<IMediaInstance>;
+    override play(source?: string | PlayOptions | CompleteCallback, callback?: CompleteCallback): IMediaInstance | Promise<IMediaInstance>;
+    override play(source?: any, callback?: any): IMediaInstance | Promise<IMediaInstance> {
+        if (typeof source === 'string') {
+            this.alias = source;
+        }
+        return super.play(source, callback);
+    }
+
+    /**
+     * https://github.com/pixijs/sound/blob/main/src/Sound.ts#L235
+     */
+    public static from(source: string | string[] | Options | ArrayBuffer | HTMLAudioElement | AudioBuffer): Sound {
+        let s = Sound.from(source);
+        return new SoundAudio(s.media, s.options);
+    }
+}
