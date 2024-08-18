dc41c53141371a35ee7c0f0619e7e7eb849e7fda
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-18 22:26:11 +0200

    DialogueInterface

---
+++ src/override/DialogueInterface.ts
@@ -0,0 +1,14 @@
+/**
+ * DialogueInterface is the type of dialogue used in the game.
+ * You can override this interface to add your own props.
+ * @example
+ * ```typescript
+ * // pixi-vn.types.ts
+ * declare module '@drincs/pixi-vn/dist/override' {
+ *     interface DialogueInterface {
+ *         color: string
+ *     }
+ * }
+ * ```
+ */
+export default interface DialogueInterface { }
