08a891b48c7f7ce957dbe7f33b6c81f8981356f8
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-19 12:20:56 +0200

    PixiVNJson

--- src/interface/pixivn-json/PixiVNJson.ts
+++ src/interface/pixivn-json/PixiVNJson.ts
@@ -1,10 +1,17 @@
 import LabelJsonType from "./LabelJsonType"
-import { SetFlagStorageJsonType } from "./StorageJsonType"
+import { SetFlagStorageJsonType, SetStorageJsonType } from "./StorageJsonType"
 
 /**
  * PixiVNJson It can be defined as a programming language to write a narrative written in json.
  */
 export default interface PixiVNJson {
-    initialVariables?: (| SetFlagStorageJsonType)[]
+    /**
+     * The initial variables are added to the system when this json is loaded.
+     * They will be set only if there are no variables with the same key already.
+     */
+    initialVariables?: (SetStorageJsonType | SetFlagStorageJsonType)[]
+    /**
+     * The labels to be used in the narrative. They will be added to the system
+     */
     labels?: LabelJsonType
 }
