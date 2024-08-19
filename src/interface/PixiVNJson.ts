7c59aadf8bb09823eb8e246726471100dc0b7f06
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-19 15:30:35 +0200

    constants

--- src/interface/PixiVNJson.ts
+++ src/interface/PixiVNJson.ts
@@ -11,6 +11,12 @@ export default interface PixiVNJson {
      */
     initialVariables?: PixiVNJsonStorageSet[]
     /**
+     * The variables are added to the system when this json is loaded.
+     * if there are variables with the same key already, they will be overwritten.
+     * This solution corresponds to the **constants**.
+     */
+    constants?: PixiVNJsonStorageSet[]
+    /**
      * The labels to be used in the narrative. They will be added to the system
      */
     labels?: PixiVNJsonLabels
