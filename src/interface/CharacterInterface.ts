d54c0daa2973b4b20b228d71b7d9fd1c11cc08c5
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-20 23:43:49 +0200

    CharacterBaseInterface

--- src/interface/CharacterInterface.ts
+++ src/interface/CharacterInterface.ts
@@ -1,4 +1,4 @@
-import CharacterInterface from "../override/CharacterInterface";
+import { CharacterInterface } from "@drincs/pixi-vn/dist/override";
 
 export default interface CharacterBaseInterface extends CharacterInterface {
     id: string
