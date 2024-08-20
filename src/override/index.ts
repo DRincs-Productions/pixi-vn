34240eb5a66cd3925ce16846b2af81f1555a5b97
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-20 23:39:58 +0200

    fast fix

--- src/override/index.ts
+++ src/override/index.ts
@@ -1,3 +1,4 @@
+export { default as CharacterInterface } from './CharacterInterface';
 export { default as StepLabelProps } from './StepLabelProps';
 export { default as StepLabelResult } from './StepLabelResult';
 
