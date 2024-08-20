926714e465ff43091619c85ee8f96bc3422d8a88
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-20 23:25:12 +0200

    tsup fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "tsup";
 
+// https://dev.to/orabazu/how-to-bundle-a-tree-shakable-typescript-library-with-tsup-and-publish-with-npm-3c46
 export default defineConfig({
     entry: ["src/**/*.ts"],
     format: ["cjs", "esm"], // Build for commonJS and ESmodules
