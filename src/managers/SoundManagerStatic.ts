23430afe54c5a1a1487792c67622951796b4b185
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-24 23:10:22 +0200

    imrpove

--- src/managers/SoundManagerStatic.ts
+++ src/managers/SoundManagerStatic.ts
@@ -2,6 +2,7 @@ import { Sound } from "../classes"
 import { ExportedSoundPlay } from "../interface/export/ExportedSounds"
 
 export default class SoundManagerStatic {
+    private constructor() { }
     static soundAliasesOrder: string[] = []
     static playInStepIndex: ExportedSoundPlay = {}
     static sounds: { [key: string]: Sound } = {}
