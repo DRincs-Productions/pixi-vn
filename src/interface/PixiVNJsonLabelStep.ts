ca453c350d02a7df07c9804a97b9cea04f542032
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-09-03 18:29:39 +0200

    PixiVNJsonLabelToOpen with value

--- src/interface/PixiVNJsonLabelStep.ts
+++ src/interface/PixiVNJsonLabelStep.ts
@@ -3,6 +3,7 @@ import { LabelIdType } from "../types/LabelIdType"
 import PixiVNJsonConditionalStatements from "./PixiVNJsonConditionalStatements"
 import { PixiVNJsonLabel } from "./PixiVNJsonLabels"
 import PixiVNJsonOperations from "./PixiVNJsonOperations"
+import { PixiVNJsonValueGet } from "./PixiVNJsonValue"
 
 export type PixiVNJsonChoice = {
     /**
@@ -45,7 +46,7 @@ export type PixiVNJsonLabelToOpen = {
     /**
      * The id of the label to open.
      */
-    label: string,
+    label: string | PixiVNJsonValueGet,
     /**
      * Label opening mode
      */
