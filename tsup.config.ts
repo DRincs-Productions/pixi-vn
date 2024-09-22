ad8ce5fdbd318ec7e13bfb2a0d34bf379467a2e3
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-09-22 17:28:32 +0200

    reset tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,12 +5,12 @@ export default defineConfig({
     entry: ["src/**/*.ts"],
     format: ["cjs", "esm"], // Build for commonJS and ESmodules
     dts: true, // Generate declaration file (.d.ts)
-    // treeshake: true,
-    // splitting: false,
-    sourcemap: true, // Generate sourcemap, it was removed because otherwise it would explode
-    // clean: true,
-    // minify: true,
-    // bundle: true,
-    // skipNodeModulesBundle: false, // Skip bundling of node_modules
+    treeshake: true,
+    splitting: false,
+    // sourcemap: true, // Generate sourcemap, it was removed because otherwise it would explode
+    clean: true,
+    minify: true,
+    bundle: true,
+    skipNodeModulesBundle: false, // Skip bundling of node_modules
     entryPoints: ['src/index.ts'],
 });
