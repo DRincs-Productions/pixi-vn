d6b5a4cb2707a841a03504b20eee408c998e2c4d
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-09-24 10:05:18 +0200

    export ICanvasVideoMemory

--- src/interface/canvas/index.ts
+++ src/interface/canvas/index.ts
@@ -3,5 +3,6 @@ export type { default as ICanvasContainerMemory } from './ICanvasContainerMemory
 export type { default as ICanvasImageMemory } from './ICanvasImageMemory';
 export type { ICanvasSpriteBaseMemory, default as ICanvasSpriteMemory } from './ICanvasSpriteMemory';
 export type { default as ICanvasTextTextMemory } from './ICanvasTextTextMemory';
+export type { default as ICanvasVideoMemory } from './ICanvasVideoMemory';
 export type { default as ITextureMemory } from './ITextureMemory';
 
