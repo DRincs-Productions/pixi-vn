5892fb1c6bacc7cb96d2f4d6a967cbf0ffc8d761
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-12-02 09:25:08 +0100

    Add ImageContainer class with dynamic image loading and update test label

--- src/classes/canvas/ImageContainer.ts
+++ src/classes/canvas/ImageContainer.ts
@@ -1,3 +1,4 @@
+import { ContainerOptions } from "pixi.js";
 import Container from "./Container";
 import ImageSprite from "./ImageSprite";
 
@@ -8,19 +9,22 @@ export const CANVAS_CONTAINER_ID = "image-container"
  * but this container is composed only of {@link ImageSprite} and introduces the {@link ImageContainer.load} functionality
  * @example
  * ```typescript
- *  const container = new ImageContainer();
- *  canvas.add(container);
- *  for (let i = 0; i < 25; i++)
- *  {
- *      const bunny = new ImageSprite({}, 'https://pixijs.com/assets/bunny.png');
- *      bunny.x = (i % 5) * 40;
- *      bunny.y = Math.floor(i / 5) * 40;
- *      container.addChild(bunny);
- *  }
+ *  const liamBodyImageUrl = 'https://example.com/assets/liam/body.png';
+ *  const liamHeadImageUrl = 'https://example.com/assets/liam/head.png';
+ *  const container = new ImageContainer(undefined, [liamBodyImageUrl, liamHeadImageUrl]);
  *  await container.load()
+ *  canvas.add(container);
  * ```
  */
 export default class ImageContainer extends Container<ImageSprite> {
+    constructor(options?: ContainerOptions<ImageSprite>, imageLinks: string[] = []) {
+        super(options)
+        if (imageLinks) {
+            imageLinks.forEach(imageLink => {
+                this.addChild(new ImageSprite(undefined, imageLink))
+            })
+        }
+    }
     override get memory() {
         let memory = super.memory
         memory.pixivnId = CANVAS_CONTAINER_ID
