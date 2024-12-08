f9b98c718dffbf03e50e25f23714811e8b400e7a
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-12-08 21:28:56 +0100

    Add alignment properties to CanvasBaseItem for improved positioning

--- src/classes/canvas/CanvasBaseItem.ts
+++ src/classes/canvas/CanvasBaseItem.ts
@@ -1,4 +1,4 @@
-import { Container as PixiContainer } from "pixi.js";
+import { Container as PixiContainer, PointData } from "pixi.js";
 import { canvasElementDecorator } from "../../decorators";
 import { getCanvasElementInstanceById } from "../../decorators/canvas-element-decorator";
 import { CanvasBaseItemMemory } from "../../interface";
@@ -39,4 +39,37 @@ export default class CanvasBaseItem<T2 extends CanvasBaseItemMemory> extends Pix
      * Get the id of the canvas element. This variable is used in the system to get the canvas element by id, {@link getCanvasElementInstanceById}
      */
     pixivnId: string = "canvas_element_id_not_set"
+    /**
+     * is a way to set the position of the element in the canvas. compared to position, align, it is a percentage used to determine the proximity from the edges of the canvas.
+     * For example, if you set align to 0.5, the element will be in the center of the canvas.
+     * If you set align to 0, the left end and a top end of the element will be in the left end and top end of the canvas.
+     * If you set align to 1, the right end and a bottom end of the element will be in the right end and bottom end of the canvas.
+     */
+    set align(value: PointData | number) {
+        if (typeof value === "number") {
+            this.x = value
+            this.y = value
+        } else {
+            this.x = value.x
+            this.y = value.y
+        }
+    }
+    /**
+     * is a way to set the position of the element in the canvas. compared to position, align, it is a percentage used to determine the proximity from the edges of the canvas.
+     * For example, if you set align to 0.5, the element will be in the center of the canvas.
+     * If you set align to 0, the left end and a top end of the element will be in the left end and top end of the canvas.
+     * If you set align to 1, the right end and a bottom end of the element will be in the right end and bottom end of the canvas.
+     */
+    set xAlign(value: number) {
+        this.x = value * (canvas.screen.width - this.width - this.pivot.x)
+    }
+    /**
+     * is a way to set the position of the element in the canvas. compared to position, align, it is a percentage used to determine the proximity from the edges of the canvas.
+     * For example, if you set align to 0.5, the element will be in the center of the canvas.
+     * If you set align to 0, the left end and a top end of the element will be in the left end and top end of the canvas.
+     * If you set align to 1, the right end and a bottom end of the element will be in the right end and bottom end of the canvas.
+     */
+    set yAlign(value: number) {
+        this.y = value * (canvas.screen.height - this.height - this.pivot.y)
+    }
 }
