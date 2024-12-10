b61d23de18cdd910221910164d035991f6208319
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-12-10 18:39:26 +0100

    Refactor setMemory method in Container to ensure memory is set after adding children

--- src/classes/canvas/Container.ts
+++ src/classes/canvas/Container.ts
@@ -41,12 +41,12 @@ export default class Container<C extends ContainerChild = ContainerChild, Memory
         this.setMemory(value)
     }
     async setMemory(value: Memory): Promise<void> {
-        setMemoryContainer(this, value)
         for (let i = 0; i < value.elements.length; i++) {
             let child = value.elements[i]
             let element = await importCanvasElement<C>(child)
             this.addChild(element)
         }
+        setMemoryContainer(this, value)
     }
 }
 
