5ae7ea729602e60466d489dccf22a81936c852ea
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-12-31 00:06:18 +0100

    Add default direction property to transition props interfaces

--- src/interface/canvas/transition-props.ts
+++ src/interface/canvas/transition-props.ts
@@ -15,18 +15,21 @@ export interface ShowWithFadeTransitionProps extends BaseTransitionProps, Omit<F
 export interface MoveInOutProps extends BaseTransitionProps, Omit<MoveTickerProps, aliasToRemoveAfterType | "startOnlyIfHaveTexture" | "destination" | "isPushInOut"> {
     /**
      * The direction of the movement.
+     * @default "right"
      */
     direction?: "up" | "down" | "left" | "right",
 }
 export interface ZoomInOutProps extends BaseTransitionProps, Omit<ZoomTickerProps, "startOnlyIfHaveTexture" | "type" | "limit" | "isZoomInOut"> {
     /**
      * The direction of the zoom effect.
+     * @default "right"
      */
     direction?: "up" | "down" | "left" | "right",
 }
 export interface PushInOutProps extends BaseTransitionProps, Omit<MoveTickerProps, "startOnlyIfHaveTexture" | "destination" | "isPushInOut"> {
     /**
      * The direction of the push effect.
+     * @default "right"
      */
     direction?: "up" | "down" | "left" | "right",
 } 
