c39786aa2857b21da5c5137f1480aa533d557df1
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-05 12:38:16 +0100

    Add getSuperPivot function to calculate pivot positions based on canvas rotation

--- src/functions/canvas/canvas-property-utility.ts
+++ src/functions/canvas/canvas-property-utility.ts
@@ -1,3 +1,4 @@
+import { Container as PixiContainer } from "pixi.js"
 import { canvas } from "../.."
 import { AdditionalPositionsExtensionProps } from "../../classes/canvas/AdditionalPositions"
 
@@ -86,4 +87,60 @@ export function calculatePercentagePositionByPosition(type: "width" | "height", 
     else {
         return position / canvas.screen.height
     }
+}
+
+export function getSuperPivot(canvasElement: PixiContainer): { x: number, y: number } {
+    let angle = canvasElement.angle % 360
+    if (angle < 0) {
+        angle += 360
+    }
+    if (angle === 0) {
+        return { x: canvasElement.pivot.x, y: canvasElement.pivot.y }
+    }
+    else if (angle === 90) {
+        return { x: - canvasElement.pivot.y, y: canvasElement.pivot.x }
+    }
+    else if (angle === 180) {
+        return { x: - canvasElement.pivot.x, y: - canvasElement.pivot.y }
+    }
+    else if (angle === 270) {
+        return { x: canvasElement.pivot.y, y: - canvasElement.pivot.x }
+    }
+    else if (angle > 0 && angle < 90) {
+        let angleRad = angle * Math.PI / 180
+        let cos = Math.cos(angleRad)
+        let sin = Math.sin(angleRad)
+        return {
+            x: canvasElement.pivot.x * cos - canvasElement.pivot.y * sin,
+            y: canvasElement.pivot.x * sin + canvasElement.pivot.y * cos
+        }
+    }
+    else if (angle > 90 && angle < 180) {
+        let angleRad = (angle - 90) * Math.PI / 180
+        let cos = Math.cos(angleRad)
+        let sin = Math.sin(angleRad)
+        return {
+            x: - canvasElement.pivot.y * sin - canvasElement.pivot.x * cos,
+            y: canvasElement.pivot.y * cos - canvasElement.pivot.x * sin
+        }
+    }
+    else if (angle > 180 && angle < 270) {
+        let angleRad = (angle - 180) * Math.PI / 180
+        let cos = Math.cos(angleRad)
+        let sin = Math.sin(angleRad)
+        return {
+            x: - canvasElement.pivot.x * cos + canvasElement.pivot.y * sin,
+            y: - canvasElement.pivot.x * sin - canvasElement.pivot.y * cos
+        }
+    }
+    else if (angle > 270 && angle < 360) {
+        let angleRad = (angle - 270) * Math.PI / 180
+        let cos = Math.cos(angleRad)
+        let sin = Math.sin(angleRad)
+        return {
+            x: canvasElement.pivot.y * sin - canvasElement.pivot.x * cos,
+            y: - canvasElement.pivot.y * cos - canvasElement.pivot.x * sin
+        }
+    }
+    return { x: 0, y: 0 }
 }
