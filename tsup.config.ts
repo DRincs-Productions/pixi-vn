1b43c711c36ec184acafe1c86ecd37a2922ff4e1
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-14 19:34:30 +0100

    Enable tree-shaking and disable code splitting in tsup configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
     entry: ["src/**/*.ts"],
     format: ["cjs", "esm"], // Build for commonJS and ESmodules
     dts: true, // Generate declaration file (.d.ts)
-    // treeshake: true,
-    // splitting: false,
+    treeshake: true,
+    splitting: false,
     // sourcemap: true, // Generate sourcemap, it was removed because otherwise it would explode
     clean: true,
     minify: true,
