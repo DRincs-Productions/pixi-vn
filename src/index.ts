444aa30f81508ef1975a5bfd68e45994099ecbcb
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-25 17:58:25 +0100

    Refactor import statements for consistency and formatting in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
 export { Assets, Rectangle } from "pixi.js";
 export type { Texture, TextureSourceLike, Ticker as TickerValue, UPDATE_PRIORITY } from "pixi.js";
-export * from './classes';
-export * from './classes/ticker';
-export { CANVAS_APP_STAGE_ALIAS, filters, Pause, Repeat } from './constants';
-export * from './decorators';
-export * from './functions';
-export * from './interface';
-export * from './labels';
-export * from './managers';
-export * from './types';
-export * from './types/ticker';
+export * from "./classes";
+export * from "./classes/ticker";
+export { CANVAS_APP_STAGE_ALIAS, filters, Pause, Repeat } from "./constants";
+export * from "./decorators";
+export * from "./functions";
+export * from "./interface";
+export * from "./labels";
+export * from "./managers";
+export * from "./types";
+export * from "./types/ticker";
 
 import { Assets, Rectangle } from "pixi.js";
-import * as classes from './classes';
-import * as classesTicker from './classes/ticker';
-import { CANVAS_APP_STAGE_ALIAS, filters, Pause, Repeat } from './constants';
-import * as decorators from './decorators';
-import * as functions from './functions';
-import * as pixivninterface from './interface';
-import * as labels from './labels';
-import * as managers from './managers';
+import * as classes from "./classes";
+import * as classesTicker from "./classes/ticker";
+import { CANVAS_APP_STAGE_ALIAS, filters, Pause, Repeat } from "./constants";
+import * as decorators from "./decorators";
+import * as functions from "./functions";
+import * as pixivninterface from "./interface";
+import * as labels from "./labels";
+import * as managers from "./managers";
 
 const pixivn = {
     Assets,
@@ -36,5 +36,4 @@ const pixivn = {
     ...labels,
     ...managers,
 };
-export default pixivn
-
+export default pixivn;
