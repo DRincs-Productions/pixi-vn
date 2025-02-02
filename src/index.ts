b3e2f39ead323fbe157bc3228916674bb22927fb
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-02-02 16:43:09 +0100

    Expand exported types in index.ts to include ContainerOptions and SpriteOptions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 export { Assets, Rectangle } from "pixi.js";
-export type { Texture, TextureSourceLike, Ticker as TickerValue, UPDATE_PRIORITY } from "pixi.js";
+export type {
+    ContainerOptions,
+    SpriteOptions,
+    TextOptions,
+    Texture,
+    TextureSourceLike,
+    Ticker as TickerValue,
+    UPDATE_PRIORITY,
+} from "pixi.js";
 export * from "./classes";
 export * from "./classes/ticker";
 export { CANVAS_APP_GAME_LAYER_ALIAS, filters, Pause, Repeat } from "./constants";
