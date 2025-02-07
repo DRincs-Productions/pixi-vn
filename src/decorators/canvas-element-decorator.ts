2ee85cd702d35904a61a00d2d738c9cf532a66c2
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-02-07 16:49:17 +0100

    Refactor canvas element decorator and import utility for improved readability and consistency

--- src/decorators/canvas-element-decorator.ts
+++ src/decorators/canvas-element-decorator.ts
@@ -1,37 +1,38 @@
-import CanvasBaseItem from "../classes/canvas/CanvasBaseItem"
-import { CanvasElementAliasType } from "../types/CanvasElementAliasType"
+import CanvasBaseItem from "../classes/canvas/CanvasBaseItem";
+import { CanvasElementAliasType } from "../types/CanvasElementAliasType";
 
-export const registeredCanvasElement: { [name: CanvasElementAliasType]: typeof CanvasBaseItem<any> } = {}
+const registeredCanvasElement: { [name: CanvasElementAliasType]: typeof CanvasBaseItem<any> } = {};
 /**
  * Is a decorator that register a canvas element in the game.
  * @param name Name of the canvas element, by default it will use the class name. If the name is already registered, it will show a warning
- * @returns 
+ * @returns
  */
 export default function canvasComponentDecorator(name?: CanvasElementAliasType) {
     return function (target: typeof CanvasBaseItem<any>) {
         if (!name) {
-            name = target.name
+            name = target.name;
         }
         if (registeredCanvasElement[name]) {
-            console.warn(`[Pixi’VN] CanvasElement ${name} already registered`)
+            console.warn(`[Pixi’VN] CanvasElement ${name} already registered`);
         }
-        target.prototype.pixivnId = name
-        registeredCanvasElement[name] = target
-    }
+        target.prototype.pixivnId = name;
+        registeredCanvasElement[name] = target;
+    };
 }
 
-export function getCanvasElementTypeById<T extends typeof CanvasBaseItem<any>>(canvasId: CanvasElementAliasType): T | undefined {
+export function getCanvasElementTypeById<T extends typeof CanvasBaseItem<any>>(
+    canvasId: CanvasElementAliasType
+): T | undefined {
     try {
-        let eventType = registeredCanvasElement[canvasId]
+        let eventType = registeredCanvasElement[canvasId];
         if (!eventType) {
-            console.error(`[Pixi’VN] CanvasElement ${canvasId} not found`)
-            return
+            console.error(`[Pixi’VN] CanvasElement ${canvasId} not found`);
+            return;
         }
-        new eventType()
-        return eventType as T
-    }
-    catch (e) {
-        console.error(`[Pixi’VN] Error while getting CanvasElement ${canvasId}`, e)
-        return
+        new eventType();
+        return eventType as T;
+    } catch (e) {
+        console.error(`[Pixi’VN] Error while getting CanvasElement ${canvasId}`, e);
+        return;
     }
 }
--- src/functions/canvas/canvas-import-utility.ts
+++ src/functions/canvas/canvas-import-utility.ts
@@ -1,6 +1,13 @@
 import { CanvasBaseItem, Container, ImageContainer, ImageSprite, Sprite, Text, VideoSprite } from "../../classes";
-import { CANVAS_CONTAINER_ID, CANVAS_IMAGE_CONTAINER_ID, CANVAS_IMAGE_ID, CANVAS_SPRITE_ID, CANVAS_TEXT_ID, CANVAS_VIDEO_ID } from "../../constants";
-import { registeredCanvasElement } from "../../decorators/canvas-element-decorator";
+import {
+    CANVAS_CONTAINER_ID,
+    CANVAS_IMAGE_CONTAINER_ID,
+    CANVAS_IMAGE_ID,
+    CANVAS_SPRITE_ID,
+    CANVAS_TEXT_ID,
+    CANVAS_VIDEO_ID,
+} from "../../constants";
+import { getCanvasElementTypeById } from "../../decorators/canvas-element-decorator";
 import { CanvasBaseItemMemory } from "../../interface";
 import { CanvasElementAliasType } from "../../types/CanvasElementAliasType";
 
@@ -9,53 +16,46 @@ import { CanvasElementAliasType } from "../../types/CanvasElementAliasType";
  * @param memory Memory object of the canvas
  * @returns Canvas element
  */
-export async function importCanvasElement<T extends CanvasBaseItem<any>>(
-    memory: CanvasBaseItemMemory,
-): Promise<T> {
-    let element = getCanvasElementInstanceById<T>(memory.pixivnId)
+export async function importCanvasElement<T extends CanvasBaseItem<any>>(memory: CanvasBaseItemMemory): Promise<T> {
+    let element = getCanvasElementInstanceById<T>(memory.pixivnId);
     if (element) {
-        await element.setMemory(memory)
-    }
-    else {
-        throw new Error("[Pixi’VN] The element " + memory.pixivnId + " could not be created")
+        await element.setMemory(memory);
+    } else {
+        throw new Error("[Pixi’VN] The element " + memory.pixivnId + " could not be created");
     }
 
-    return element
+    return element;
 }
 
-export function getCanvasElementInstanceById<T extends CanvasBaseItem<any>>(canvasId: CanvasElementAliasType): T | undefined {
+export function getCanvasElementInstanceById<T extends CanvasBaseItem<any>>(
+    canvasId: CanvasElementAliasType
+): T | undefined {
     try {
-        let eventType = registeredCanvasElement[canvasId]
+        let eventType = getCanvasElementTypeById(canvasId);
         if (!eventType) {
             if (canvasId === CANVAS_CONTAINER_ID) {
-                eventType = Container
-            }
-            else if (canvasId === CANVAS_VIDEO_ID) {
-                eventType = VideoSprite
-            }
-            else if (canvasId === CANVAS_IMAGE_ID) {
-                eventType = ImageSprite
-            }
-            else if (canvasId === CANVAS_SPRITE_ID) {
-                eventType = Sprite
-            }
-            else if (canvasId === CANVAS_TEXT_ID) {
-                eventType = Text
-            }
-            else if (canvasId === CANVAS_IMAGE_CONTAINER_ID) {
-                eventType = ImageContainer
+                eventType = Container;
+            } else if (canvasId === CANVAS_VIDEO_ID) {
+                eventType = VideoSprite;
+            } else if (canvasId === CANVAS_IMAGE_ID) {
+                eventType = ImageSprite;
+            } else if (canvasId === CANVAS_SPRITE_ID) {
+                eventType = Sprite;
+            } else if (canvasId === CANVAS_TEXT_ID) {
+                eventType = Text;
+            } else if (canvasId === CANVAS_IMAGE_CONTAINER_ID) {
+                eventType = ImageContainer;
             }
         }
 
         if (!eventType) {
-            console.error(`[Pixi’VN] CanvasElement ${canvasId} not found`)
-            return
+            console.error(`[Pixi’VN] CanvasElement ${canvasId} not found`);
+            return;
         }
-        let canvasElement = new eventType()
-        return canvasElement as T
-    }
-    catch (e) {
-        console.error(`[Pixi’VN] Error while getting CanvasElement ${canvasId}`, e)
-        return
+        let canvasElement = new eventType();
+        return canvasElement as T;
+    } catch (e) {
+        console.error(`[Pixi’VN] Error while getting CanvasElement ${canvasId}`, e);
+        return;
     }
 }
