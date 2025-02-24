39a3e494a648c5a1b8ed21dff5d2fc4c3e5ce03a
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-02-24 10:00:04 +0100

    Add StorageManagerInterface and NarrationManagerInterface interfaces and update StorageManager to implement them

---
+++ src/interface/managers/StorageManagerInterface.ts
@@ -0,0 +1,88 @@
+import { CacheableMemory } from "cacheable";
+import { StorageManagerStatic } from "../../managers";
+import { StorageElementType } from "../../types";
+import ExportedStorage from "../export/ExportedStorage";
+
+export default interface StorageManagerInterface {
+    /**
+     * Get the CacheableMemory storage.
+     * CacheableMemory is a cache system that can be used to store data in memory.
+     * You can read more about it here: https://cacheable.org/docs/cacheable/#cacheablememory---in-memory-cache
+     */
+    readonly storage: CacheableMemory;
+    /**
+     * System-reserved storage keys
+     */
+    readonly keysSystem: typeof StorageManagerStatic._keysSystem;
+    /**
+     * Set a variable in the storage
+     * @param key The key of the variable
+     * @param value The value of the variable. If undefined, the variable will be removed
+     * @returns
+     */
+    setVariable(key: string, value: StorageElementType): void;
+    /**
+     * Get a variable from the storage. If the variable is a temporary variable, it will return the temporary variable
+     * @param key The key of the variable
+     * @returns The value of the variable. If the variable does not exist, it will return undefined
+     */
+    getVariable<T extends StorageElementType>(key: string): T | undefined;
+    /**
+     * Remove a variable from the storage
+     * @param key The key of the variable
+     * @returns
+     */
+    removeVariable(key: string): void;
+    /**
+     * Set a variable in the temporary storage. The lifespan of the variable is the number of opened labels.
+     * To get the temporary variable, use {@link this.getVariable}
+     * @param key The key of the temporary variable
+     * @param value The value of the temporary variable. If undefined, the variable will be removed
+     * @returns
+     */
+    setTempVariable(key: string, value: StorageElementType): void;
+    /**
+     * Remove a temporary variable
+     * @param key The key of the temporary variable
+     */
+    removeTempVariable(key: string): void;
+
+    /**
+     * Set a flag to true or false.
+     * @param name The name of the flag
+     * @param value The value of the flag.
+     */
+    setFlag(name: string, value: boolean): void;
+    /**
+     * Get the value of a flag
+     * @param name The name of the flag
+     * @returns The value of the flag
+     */
+    getFlag(name: string): boolean;
+
+    /**
+     * Clear the storage and the oidsUsed
+     * @returns
+     */
+    clear(): void;
+    /**
+     * Export the storage to a JSON string
+     * @returns The JSON string
+     */
+    exportJson(): string;
+    /**
+     * Export the storage to an object
+     * @returns The object
+     */
+    export(): ExportedStorage;
+    /**
+     * Import the storage from a JSON string
+     * @param dataString The JSON string
+     */
+    importJson(dataString: string): void;
+    /**
+     * Import the storage from an object
+     * @param data The object
+     */
+    import(data: object): void;
+}
--- src/managers/StorageManager.ts
+++ src/managers/StorageManager.ts
@@ -1,20 +1,17 @@
 import { narration } from ".";
 import { createExportableElement } from "../functions/export-utility";
 import { logger } from "../functions/log-utility";
-import { ExportedStorage } from "../interface";
+import { ExportedStorage, StorageManagerInterface } from "../interface";
 import { StorageElementType } from "../types/StorageElementType";
 import StorageManagerStatic from "./StorageManagerStatic";
 
-export default class StorageManager {
+export default class StorageManager implements StorageManagerInterface {
+    get storage() {
+        return StorageManagerStatic._storage;
+    }
     get keysSystem() {
         return StorageManagerStatic._keysSystem;
     }
-    /**
-     * Set a variable in the storage
-     * @param key The key of the variable
-     * @param value The value of the variable. If undefined, the variable will be removed
-     * @returns
-     */
     public setVariable(key: string, value: StorageElementType) {
         key = key.toLowerCase();
         if (value === undefined || value === null) {
@@ -23,11 +20,6 @@ export default class StorageManager {
         }
         StorageManagerStatic._storage.set(key, value);
     }
-    /**
-     * Get a variable from the storage. If the variable is a temporary variable, it will return the temporary variable
-     * @param key The key of the variable
-     * @returns The value of the variable. If the variable does not exist, it will return undefined
-     */
     public getVariable<T extends StorageElementType>(key: string): T | undefined {
         key = key.toLowerCase();
         if (StorageManagerStatic.tempStorage.hasOwnProperty(key)) {
@@ -35,24 +27,12 @@ export default class StorageManager {
         }
         return createExportableElement(StorageManagerStatic._storage.get<T>(key));
     }
-    /**
-     * Remove a variable from the storage
-     * @param key The key of the variable
-     * @returns
-     */
     public removeVariable(key: string) {
         key = key.toLowerCase();
         if (StorageManagerStatic._storage.has(key)) {
             StorageManagerStatic._storage.delete(key);
         }
     }
-    /**
-     * Set a variable in the temporary storage. The lifespan of the variable is the number of opened labels.
-     * To get the temporary variable, use {@link this.getVariable}
-     * @param key The key of the temporary variable
-     * @param value The value of the temporary variable. If undefined, the variable will be removed
-     * @returns
-     */
     public setTempVariable(key: string, value: StorageElementType) {
         key = key.toLowerCase();
         let tempStorage = StorageManagerStatic.tempStorage;
@@ -69,10 +49,6 @@ export default class StorageManager {
         StorageManagerStatic.tempStorage = tempStorage;
         StorageManagerStatic.tempStorageDeadlines = tempStorageDeadlines;
     }
-    /**
-     * Remove a temporary variable
-     * @param key The key of the temporary variable
-     */
     public removeTempVariable(key: string) {
         key = key.toLowerCase();
         let tempStorage = StorageManagerStatic.tempStorage;
@@ -84,12 +60,6 @@ export default class StorageManager {
         StorageManagerStatic.tempStorage = tempStorage;
         StorageManagerStatic.tempStorageDeadlines = tempStorageDeadlines;
     }
-
-    /**
-     * Set a flag to true or false.
-     * @param name The name of the flag
-     * @param value The value of the flag.
-     */
     setFlag(name: string, value: boolean) {
         let flags = this.getVariable<string[]>(this.keysSystem.FLAGS_CATEGORY_KEY) || [];
         if (value) {
@@ -104,21 +74,10 @@ export default class StorageManager {
         }
         this.setVariable(this.keysSystem.FLAGS_CATEGORY_KEY, flags);
     }
-
-    /**
-     * Get the value of a flag
-     * @param name The name of the flag
-     * @returns The value of the flag
-     */
     getFlag(name: string): boolean {
         let flags = this.getVariable<string[]>(this.keysSystem.FLAGS_CATEGORY_KEY) || [];
         return flags.includes(name);
     }
-
-    /**
-     * Clear the storage and the oidsUsed
-     * @returns
-     */
     public clear() {
         StorageManagerStatic._storage.clear();
         StorageManagerStatic._storage.setMany(StorageManagerStatic._baseStorage);
