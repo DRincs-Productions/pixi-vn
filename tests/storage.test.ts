f477b552278a8e44b4cfeac5a3c53e3fcf45d7a6
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-02-26 09:07:33 +0100

    Update storage tests to validate case sensitivity in variable handling

--- tests/storage.test.ts
+++ tests/storage.test.ts
@@ -4,10 +4,13 @@ import { storage } from "../src";
 test("setVariable & getVariable", async () => {
     storage.setVariable("test", "test1");
     expect(storage.getVariable("test")).toBe("test1");
+    expect(storage.getVariable("Test")).toBe("test1");
 
     storage.setVariable("Test", "test2");
+    expect(storage.getVariable("test")).toBe(undefined);
     expect(storage.getVariable("Test")).toBe("test2");
 
     storage.setVariable("test", "test3");
-    expect(storage.getVariable("test")).toBe(undefined);
+    expect(storage.getVariable("test")).toBe("test3");
+    expect(storage.getVariable("Test")).toBe("test2");
 });
