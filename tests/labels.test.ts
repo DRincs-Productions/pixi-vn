d8cfc775f333808f67c02ae75bdfb344064cb067
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-03-06 22:53:36 +0100

    Add unit tests for NarrationManager label functionality

---
+++ tests/labels.test.ts
@@ -0,0 +1,80 @@
+import { expect, test } from "vitest";
+import {
+    createExportableElement,
+    HistoryStepData,
+    NarrationManagerInterface,
+    NarrationManagerStatic,
+    newLabel,
+    sound,
+    storage,
+} from "../src";
+import { getGamePath } from "../src/functions/path-utility";
+import NarrationManager from "../src/managers/NarrationManager";
+
+const getCurrentStepData: () => HistoryStepData = () => {
+    let currentStepData: HistoryStepData = {
+        path: getGamePath(),
+        storage: storage.export(),
+        canvas: {} as any,
+        sound: sound.removeOldSoundAndExport(),
+        labelIndex: NarrationManagerStatic.currentLabelStepIndex || 0,
+        openedLabels: createExportableElement(NarrationManagerStatic._openedLabels),
+    };
+    return currentStepData;
+};
+
+const restoreFromHistoryStep: (
+    restoredStep: HistoryStepData,
+    navigate: (path: string) => void
+) => Promise<void> = async (restoredStep: HistoryStepData, navigate: (path: string) => void) => {
+    NarrationManagerStatic._originalStepData = restoredStep;
+    NarrationManagerStatic._openedLabels = createExportableElement(restoredStep.openedLabels);
+    storage.import(createExportableElement(restoredStep.storage));
+    sound.import(createExportableElement(restoredStep.sound), NarrationManagerStatic._lastStepIndex - 1);
+    navigate(restoredStep.path);
+};
+
+const forceCompletionOfTicker = () => {};
+
+const narration: NarrationManagerInterface = new NarrationManager(
+    getCurrentStepData,
+    restoreFromHistoryStep,
+    forceCompletionOfTicker
+);
+
+const pathLabel = newLabel("path", [
+    () => {
+        narration.dialogue = "This is a test label";
+    },
+    () => {
+        narration.dialogue = "This is a test label 2";
+        window.history.pushState({}, "test", "/test");
+    },
+    () => {
+        narration.dialogue = "This is a test label 3";
+    },
+]);
+
+test("path test", async () => {
+    await narration.callLabel(pathLabel, {});
+    expect(narration.dialogue).toEqual({
+        text: "This is a test label",
+        oltherParams: {},
+    });
+    await narration.goNext({});
+    expect(narration.dialogue).toEqual({
+        text: "This is a test label 2",
+        oltherParams: {},
+    });
+    await narration.goNext({});
+    expect(narration.dialogue).toEqual({
+        text: "This is a test label 3",
+        oltherParams: {},
+    });
+    expect(getGamePath()).toBe("/test");
+    await narration.goBack((path) => window.history.pushState({}, "test", path));
+    await narration.goBack((path) => window.history.pushState({}, "test", path));
+    expect(getGamePath()).toBe("/test");
+    await narration.goBack((path) => window.history.pushState({}, "test", path));
+    expect(getGamePath()).toBe("/");
+});
