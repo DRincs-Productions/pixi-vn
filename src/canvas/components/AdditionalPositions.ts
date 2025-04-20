88349858db8dc2e0937052d96ec4badfbb14c486
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-04-20 12:20:46 +0200

    Refactor: standardize formatting and improve code consistency in AdditionalPositionsExtensionProps

--- src/canvas/components/AdditionalPositions.ts
+++ src/canvas/components/AdditionalPositions.ts
@@ -7,70 +7,70 @@ export interface AdditionalPositionsExtensionProps {
      * - if you set align to 0.5, the element will be in the center of the canvas.
      * - if you set align to 0, the left end and a top end of the element will be in the left end and top end of the canvas.
      * - if you set align to 1, the right end and a bottom end of the element will be in the right end and bottom end of the canvas.
-     * 
+     *
      * **Important:** The {@link PixiContainer.pivot} field does not affect the alignment.
      */
-    align?: Partial<PointData> | number
+    align?: Partial<PointData> | number;
     /**
      * is a way to set the position of the element in the canvas. compared to position, align, it is a percentage used to determine the proximity from the edges of the canvas.
      * For example:
      * - if you set align to 0.5, the element will be in the center of the canvas.
      * - if you set align to 0, the left end and a top end of the element will be in the left end and top end of the canvas.
      * - if you set align to 1, the right end and a bottom end of the element will be in the right end and bottom end of the canvas.
-     * 
+     *
      * **Important:** The {@link PixiContainer.pivot} field does not affect the alignment.
      */
-    xAlign?: number
+    xAlign?: number;
     /**
      * is a way to set the position of the element in the canvas. compared to position, align, it is a percentage used to determine the proximity from the edges of the canvas.
      * For example:
      * - if you set align to 0.5, the element will be in the center of the canvas.
      * - if you set align to 0, the left end and a top end of the element will be in the left end and top end of the canvas.
      * - if you set align to 1, the right end and a bottom end of the element will be in the right end and bottom end of the canvas.
-     * 
+     *
      * **Important:** The {@link PixiContainer.pivot} field does not affect the alignment.
      */
-    yAlign?: number
+    yAlign?: number;
     /**
      * is a way to set the position of the element in the canvas calculated in percentage.
      * For example, if you set the {@link PixiContainer.pivot} to 0.5, and:
      * - if you set percentagePosition to 0.5, the element will be in the center of the canvas.
      * - If you set percentagePosition to 0, the center of the element will be in the left end and top end of the canvas.
      * - If you set percentagePosition to 1, the center of the element will be in the right end and bottom end of the canvas.
-     * 
+     *
      * **Important:** The {@link PixiContainer.pivot} field does affect the percentagePosition.
      */
-    percentagePosition?: Partial<PointData> | number
+    percentagePosition?: Partial<PointData> | number;
     /**
      * is a way to set the position of the element in the canvas calculated in percentage.
      * For example, if you set the {@link PixiContainer.pivot} to 0.5, and:
      * - if you set percentagePosition to 0.5, the element will be in the center of the canvas.
      * - If you set percentagePosition to 0, the center of the element will be in the left end and top end of the canvas.
      * - If you set percentagePosition to 1, the center of the element will be in the right end and bottom end of the canvas.
-     * 
+     *
      * **Important:** The {@link PixiContainer.pivot} field does affect the percentagePosition.
      */
-    xPercentagePosition?: number
+    xPercentagePosition?: number;
     /**
      * is a way to set the position of the element in the canvas calculated in percentage.
      * For example, if you set the {@link PixiContainer.pivot} to 0.5, and:
      * - if you set percentagePosition to 0.5, the element will be in the center of the canvas.
      * - If you set percentagePosition to 0, the center of the element will be in the left end and top end of the canvas.
      * - If you set percentagePosition to 1, the center of the element will be in the right end and bottom end of the canvas.
-     * 
+     *
      * **Important:** The {@link PixiContainer.pivot} field does affect the percentagePosition.
      */
-    yPercentagePosition?: number
+    yPercentagePosition?: number;
 }
 
-export default class AdditionalPositionsExtension extends PixiContainer {
+export default class AdditionalPositionsExtension {
     /**
      * is a way to set the position of the element in the canvas. compared to position, align, it is a percentage used to determine the proximity from the edges of the canvas.
      * For example:
      * - if you set align to 0.5, the element will be in the center of the canvas.
      * - if you set align to 0, the left end and a top end of the element will be in the left end and top end of the canvas.
      * - if you set align to 1, the right end and a bottom end of the element will be in the right end and bottom end of the canvas.
-     * 
+     *
      * **Important:** The {@link PixiContainer.pivot} field does not affect the alignment.
      */
     set align(_value: Partial<PointData> | number) {
@@ -85,7 +85,7 @@ export default class AdditionalPositionsExtension extends PixiContainer {
      * - if you set align to 0.5, the element will be in the center of the canvas.
      * - if you set align to 0, the left end and a top end of the element will be in the left end and top end of the canvas.
      * - if you set align to 1, the right end and a bottom end of the element will be in the right end and bottom end of the canvas.
-     * 
+     *
      * **Important:** The {@link PixiContainer.pivot} field does not affect the alignment.
      */
     set xAlign(_value: number) {
@@ -100,7 +100,7 @@ export default class AdditionalPositionsExtension extends PixiContainer {
      * - if you set align to 0.5, the element will be in the center of the canvas.
      * - if you set align to 0, the left end and a top end of the element will be in the left end and top end of the canvas.
      * - if you set align to 1, the right end and a bottom end of the element will be in the right end and bottom end of the canvas.
-     * 
+     *
      * **Important:** The {@link PixiContainer.pivot} field does not affect the alignment.
      */
     set yAlign(_value: number) {
@@ -115,7 +115,7 @@ export default class AdditionalPositionsExtension extends PixiContainer {
      * - if you set percentagePosition to 0.5, the element will be in the center of the canvas.
      * - If you set percentagePosition to 0, the center of the element will be in the left end and top end of the canvas.
      * - If you set percentagePosition to 1, the center of the element will be in the right end and bottom end of the canvas.
-     * 
+     *
      * **Important:** The {@link PixiContainer.pivot} field does affect the percentagePosition.
      */
     set percentagePosition(_value: Partial<PointData> | number) {
@@ -130,7 +130,7 @@ export default class AdditionalPositionsExtension extends PixiContainer {
      * - if you set percentagePosition to 0.5, the element will be in the center of the canvas.
      * - If you set percentagePosition to 0, the center of the element will be in the left end and top end of the canvas.
      * - If you set percentagePosition to 1, the center of the element will be in the right end and bottom end of the canvas.
-     * 
+     *
      * **Important:** The {@link PixiContainer.pivot} field does affect the percentagePosition.
      */
     set xPercentagePosition(_value: number) {
@@ -145,7 +145,7 @@ export default class AdditionalPositionsExtension extends PixiContainer {
      * - if you set percentagePosition to 0.5, the element will be in the center of the canvas.
      * - If you set percentagePosition to 0, the center of the element will be in the left end and top end of the canvas.
      * - If you set percentagePosition to 1, the center of the element will be in the right end and bottom end of the canvas.
-     * 
+     *
      * **Important:** The {@link PixiContainer.pivot} field does affect the percentagePosition.
      */
     set yPercentagePosition(_value: number) {
@@ -157,58 +157,54 @@ export default class AdditionalPositionsExtension extends PixiContainer {
     get positionType(): "pixel" | "percentage" | "align" {
         throw new Error("Method not implemented.");
     }
-    get positionInfo(): { x: number, y: number, type: "pixel" | "percentage" | "align" } {
+    get positionInfo(): { x: number; y: number; type: "pixel" | "percentage" | "align" } {
         throw new Error("Method not implemented.");
     }
 }
 
 export function analizePositionsExtensionProps<T extends AdditionalPositionsExtensionProps>(props?: T): T | undefined {
     if (!props) {
-        return props
+        return props;
     }
     if (typeof props.align !== "number") {
         if (props.xAlign != undefined) {
             if (props.align === undefined) {
-                props.align = { x: props.xAlign }
-                delete props.xAlign
-            }
-            else {
-                props.align.x = props.xAlign
-                delete props.xAlign
+                props.align = { x: props.xAlign };
+                delete props.xAlign;
+            } else {
+                props.align.x = props.xAlign;
+                delete props.xAlign;
             }
         }
         if (props.yAlign != undefined) {
             if (props.align === undefined) {
-                props.align = { y: props.yAlign }
-                delete props.yAlign
-            }
-            else {
-                props.align.y = props.yAlign
-                delete props.yAlign
+                props.align = { y: props.yAlign };
+                delete props.yAlign;
+            } else {
+                props.align.y = props.yAlign;
+                delete props.yAlign;
             }
         }
     }
     if (typeof props.percentagePosition !== "number") {
         if (props.xPercentagePosition != undefined) {
             if (props.percentagePosition === undefined) {
-                props.percentagePosition = { x: props.xPercentagePosition }
-                delete props.xPercentagePosition
-            }
-            else {
-                props.percentagePosition.x = props.xPercentagePosition
-                delete props.xPercentagePosition
+                props.percentagePosition = { x: props.xPercentagePosition };
+                delete props.xPercentagePosition;
+            } else {
+                props.percentagePosition.x = props.xPercentagePosition;
+                delete props.xPercentagePosition;
             }
         }
         if (props.yPercentagePosition != undefined) {
             if (props.percentagePosition === undefined) {
-                props.percentagePosition = { y: props.yPercentagePosition }
-                delete props.yPercentagePosition
-            }
-            else {
-                props.percentagePosition.y = props.yPercentagePosition
-                delete props.yPercentagePosition
+                props.percentagePosition = { y: props.yPercentagePosition };
+                delete props.yPercentagePosition;
+            } else {
+                props.percentagePosition.y = props.yPercentagePosition;
+                delete props.yPercentagePosition;
             }
         }
     }
-    return props
+    return props;
 }
