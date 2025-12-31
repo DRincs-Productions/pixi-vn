39bbe857d1931329ed76be7178c555e4b3d43ca1
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-12-31 16:41:31 +0100

    Refactor GameUnifier initialization in test setup for improved readability

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,4 +1,3 @@
-import { test } from "vitest";
 import {
     GameUnifier,
     HistoryManagerStatic,
@@ -13,59 +12,57 @@ import {
 } from "../src";
 import { getGamePath } from "../src/utils/path-utility";
 
-test("setup", async () => {
-    GameUnifier.init({
-        navigate: (path: string) => window.history.pushState({}, "test", path),
-        getCurrentGameStepState: () => {
-            return {
-                path: getGamePath(),
-                storage: storage.export(),
-                canvas: () => {
-                    return {} as any;
-                },
-                sound: sound.export(),
-                labelIndex: NarrationManagerStatic.currentLabelStepIndex || 0,
-                openedLabels: narration.openedLabels,
-            };
-        },
-        restoreGameStepState: async (state, navigate) => {
-            HistoryManagerStatic._originalStepData = state;
-            NarrationManagerStatic.openedLabels = state.openedLabels;
-            storage.restore(state.storage);
-            // await canvas.restore(state.canvas);
-            sound.restore(state.sound);
-            navigate(state.path);
-        },
-        // narration
-        getStepCounter: () => narration.stepCounter,
-        setStepCounter: (value) => {
-            NarrationManagerStatic._stepCounter = value;
-        },
-        getOpenedLabels: () => narration.openedLabels.length,
-        addHistoryItem: (historyInfo, options) => {
-            return stepHistory.add(historyInfo, options);
-        },
-        getCharacter: (id: string) => {
-            return RegisteredCharacters.get(id);
-        },
-        processNavigationRequests: (navigationRequestsCount: number) => {
-            let newValue = navigationRequestsCount;
-            let result: Promise<void | StepLabelResultType> = Promise.resolve();
-            if (navigationRequestsCount > 0) {
-                newValue--;
-                result = narration.continue({});
-            } else if (navigationRequestsCount < 0) {
-                newValue = 0;
-                result = stepHistory.back({}, { steps: navigationRequestsCount * -1 });
-            }
-            return { newValue, result };
-        },
-        // storage
-        getVariable: (key) => storage.get(key),
-        setVariable: (key, value) => storage.set(key, value),
-        removeVariable: (key) => storage.remove(key),
-        getFlag: (key) => storage.getFlag(key),
-        setFlag: (name, value) => storage.setFlag(name, value),
-        onLabelClosing: (openedLabelsNumber) => StorageManagerStatic.clearOldTempVariables(openedLabelsNumber),
-    });
+GameUnifier.init({
+    navigate: (path: string) => window.history.pushState({}, "test", path),
+    getCurrentGameStepState: () => {
+        return {
+            path: getGamePath(),
+            storage: storage.export(),
+            canvas: () => {
+                return {} as any;
+            },
+            sound: sound.export(),
+            labelIndex: NarrationManagerStatic.currentLabelStepIndex || 0,
+            openedLabels: narration.openedLabels,
+        };
+    },
+    restoreGameStepState: async (state, navigate) => {
+        HistoryManagerStatic._originalStepData = state;
+        NarrationManagerStatic.openedLabels = state.openedLabels;
+        storage.restore(state.storage);
+        // await canvas.restore(state.canvas);
+        sound.restore(state.sound);
+        navigate(state.path);
+    },
+    // narration
+    getStepCounter: () => narration.stepCounter,
+    setStepCounter: (value) => {
+        NarrationManagerStatic._stepCounter = value;
+    },
+    getOpenedLabels: () => narration.openedLabels.length,
+    addHistoryItem: (historyInfo, options) => {
+        return stepHistory.add(historyInfo, options);
+    },
+    getCharacter: (id: string) => {
+        return RegisteredCharacters.get(id);
+    },
+    processNavigationRequests: (navigationRequestsCount: number) => {
+        let newValue = navigationRequestsCount;
+        let result: Promise<void | StepLabelResultType> = Promise.resolve();
+        if (navigationRequestsCount > 0) {
+            newValue--;
+            result = narration.continue({});
+        } else if (navigationRequestsCount < 0) {
+            newValue = 0;
+            result = stepHistory.back({}, { steps: navigationRequestsCount * -1 });
+        }
+        return { newValue, result };
+    },
+    // storage
+    getVariable: (key) => storage.get(key),
+    setVariable: (key, value) => storage.set(key, value),
+    removeVariable: (key) => storage.remove(key),
+    getFlag: (key) => storage.getFlag(key),
+    setFlag: (name, value) => storage.setFlag(name, value),
+    onLabelClosing: (openedLabelsNumber) => StorageManagerStatic.clearOldTempVariables(openedLabelsNumber),
 });
