6bb4e3685213145057e37aff3bd381d12517923a
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-03-06 10:40:02 +0100

    Refactor motion module: remove unused timeline export from utils

--- src/motion/index.ts
+++ src/motion/index.ts
@@ -13,7 +13,7 @@ export type {
     SequenceOptions,
 } from "./interfaces/AnimationOptions";
 export { default as motion } from "./motion";
-export { animate, motionDriver, SegmentOptions, timeline } from "./utils";
+export { animate, motionDriver, SegmentOptions } from "./utils";
 
 RegisteredTickers.add(MotionTicker, "motion");
 RegisteredTickers.add(MotionSequenceTicker, "motion-sequence");
