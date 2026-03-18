ca92041931140b00f405768501418c2b5343df67
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-03-18 18:37:55 +0100

    Remove AudioChannel export and tidy up SoundOptions exports in index.ts

--- src/sound/index.ts
+++ src/sound/index.ts
@@ -1,7 +1,6 @@
 import SoundManagerInterface from "./interfaces/SoundManagerInterface";
 import SoundManager from "./SoundManager";
 
-export { default as AudioChannel } from "./classes/AudioChannel";
 export { filters } from "./constants";
 export type { default as AudioChannelInterface } from "./interfaces/AudioChannelInterface";
 export type { default as IMediaInstance } from "./interfaces/IMediaInstance";
@@ -11,7 +10,12 @@ export type {
     default as SoundGameState,
     SoundPlay,
 } from "./interfaces/SoundGameState";
-export type { ChannelOptions, default as SoundOptions, SoundPlayOptions } from "./interfaces/SoundOptions";
+export type {
+    ChannelOptions,
+    default as SoundOptions,
+    SoundPlayOptions,
+    SoundPlayOptionsWithChannel,
+} from "./interfaces/SoundOptions";
 export { default as SoundManagerStatic } from "./SoundManagerStatic";
 export type { default as SoundFilterMemory } from "./types/SoundFilterMemory";
 export { sound };
