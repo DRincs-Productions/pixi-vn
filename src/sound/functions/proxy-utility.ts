f863c6a536d6252b58400251c4b646992ce31843
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-03-27 14:57:39 +0100

    Fix optional chaining for media instance options in proxyMedia function

--- src/sound/functions/proxy-utility.ts
+++ src/sound/functions/proxy-utility.ts
@@ -9,31 +9,33 @@ export function proxyMedia(soundAlias: string, media: IMediaInstance, channel: A
             switch (prop) {
                 case "volume":
                 case "muted":
-                    return SoundManagerStatic.mediaInstances[soundAlias].options[prop];
+                    return SoundManagerStatic.mediaInstances[soundAlias]?.options[prop];
                 case "paused":
                 default:
                     return Reflect.get(target, prop, receiver);
             }
         },
         set(target, prop, value, receiver) {
-            switch (prop) {
-                case "volume":
-                    SoundManagerStatic.mediaInstances[soundAlias].options[prop] = value;
-                    value = calculateVolume(value, channel.channelOptions.volume);
-                    return Reflect.set(target, prop, value, receiver);
-                case "muted":
-                    SoundManagerStatic.mediaInstances[soundAlias].options[prop] = value;
-                    if (channel.channelOptions.muted) {
-                        return Reflect.set(target, prop, true, receiver);
-                    } else {
+            if (soundAlias in SoundManagerStatic.mediaInstances) {
+                switch (prop) {
+                    case "volume":
+                        SoundManagerStatic.mediaInstances[soundAlias].options[prop] = value;
+                        value = calculateVolume(value, channel.channelOptions.volume);
                         return Reflect.set(target, prop, value, receiver);
-                    }
-                case "loop":
-                    SoundManagerStatic.mediaInstances[soundAlias].options[prop] = value;
-                default:
-                    return Reflect.set(target, prop, value, receiver);
+                    case "muted":
+                        SoundManagerStatic.mediaInstances[soundAlias].options[prop] = value;
+                        if (channel.channelOptions.muted) {
+                            return Reflect.set(target, prop, true, receiver);
+                        } else {
+                            return Reflect.set(target, prop, value, receiver);
+                        }
+                    case "loop":
+                        SoundManagerStatic.mediaInstances[soundAlias].options[prop] = value;
+                    default:
+                        return Reflect.set(target, prop, value, receiver);
+                }
             }
-            return true;
+            return Reflect.set(target, prop, value, receiver);
         },
     });
 }
