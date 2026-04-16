abc825182f9f544ecb96280bed815eb6b9ce5ab0
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-04-16 12:07:49 +0200

    fix: update package dependencies and improve tsconfig paths for tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    plugins: [tsconfigPaths()],
+    plugins: [tsconfigPaths({ projects: ["./tsconfig.json", "./tests/tsconfig.json"] })],
     test: {
         environment: "jsdom",
-        setupFiles: ["./tests/setup.ts"],
+        setupFiles: ["tests/setup.ts"],
     },
 });
