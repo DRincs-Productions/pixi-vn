bf3a306975048b2bfe6d7400737f0e1bee2cf61d
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-05-31 11:53:33 +0200

    fix: bump version to 1.8.11 in package.json and update imports in index.ts

--- src/narration/index.ts
+++ src/narration/index.ts
@@ -1,34 +1,37 @@
-import type { NarrationManagerInterface } from "..";
-import NarrationManager from "./NarrationManager";
-
-export { newChoiceOption } from "./classes/ChoiceMenuOption";
-export { newCloseChoiceOption } from "./classes/CloseChoiceOption";
-export { default as Label } from "./classes/Label";
-export { default as LabelAbstract } from "./classes/LabelAbstract";
-export { default as newLabel } from "./decorators/newLabel";
-export { default as RegisteredLabels } from "./decorators/RegisteredLabels";
-export type { default as ChoiceInterface } from "./interfaces/ChoiceInterface";
-export type { default as DialogueInterface } from "./interfaces/DialogueInterface";
-export type { default as HistoryStep } from "./interfaces/HistoryStep";
-export type { default as LabelProps } from "./interfaces/LabelProps";
-export type { default as NarrationGameState } from "./interfaces/NarrationGameState";
-export type { default as NarrationHistory } from "./interfaces/NarrationHistory";
-export type { default as NarrationManagerInterface } from "./interfaces/NarrationManagerInterface";
-export type { default as OpenedLabel } from "./interfaces/OpenedLabel";
-export type { default as StepLabelProps } from "./interfaces/StepLabelProps";
-export type { default as StepLabelResult } from "./interfaces/StepLabelResult";
+import type NarrationManagerInterface from "@narration/interfaces/NarrationManagerInterface";
+import NarrationManager from "@narration/NarrationManager";
+export { newChoiceOption } from "@narration/classes/ChoiceMenuOption";
+export { newCloseChoiceOption } from "@narration/classes/CloseChoiceOption";
+export { default as Label } from "@narration/classes/Label";
+export { default as LabelAbstract } from "@narration/classes/LabelAbstract";
+export { default as newLabel } from "@narration/decorators/newLabel";
+export { default as RegisteredLabels } from "@narration/decorators/RegisteredLabels";
+export type { default as ChoiceInterface } from "@narration/interfaces/ChoiceInterface";
+export type { default as DialogueInterface } from "@narration/interfaces/DialogueInterface";
+export type { default as HistoryStep } from "@narration/interfaces/HistoryStep";
+export type { default as LabelProps } from "@narration/interfaces/LabelProps";
+export type { default as NarrationGameState } from "@narration/interfaces/NarrationGameState";
+export type { default as NarrationHistory } from "@narration/interfaces/NarrationHistory";
+export type { default as NarrationManagerInterface } from "@narration/interfaces/NarrationManagerInterface";
+export type { default as OpenedLabel } from "@narration/interfaces/OpenedLabel";
+export type { default as StepLabelProps } from "@narration/interfaces/StepLabelProps";
+export type { default as StepLabelResult } from "@narration/interfaces/StepLabelResult";
 export type {
     ChoiceOptionInterface,
     CloseChoiceOptionInterface,
     default as StoredChoiceInterface,
     StoredIndexedChoiceInterface,
-} from "./interfaces/StoredChoiceInterface";
-export { default as NarrationManagerStatic } from "./NarrationManagerStatic";
-export type { default as HistoryChoiceMenuOption } from "./types/HistoryChoiceMenuOption";
-export type { InputInfo } from "./types/InputInfo";
-export type { default as LabelRunModeType } from "./types/LabelRunModeType";
-export type { default as LabelSteps } from "./types/LabelSteps";
-export type { StepLabelPropsType, StepLabelResultType, StepLabelType } from "./types/StepLabelType";
+} from "@narration/interfaces/StoredChoiceInterface";
+export { default as NarrationManagerStatic } from "@narration/NarrationManagerStatic";
+export type { default as HistoryChoiceMenuOption } from "@narration/types/HistoryChoiceMenuOption";
+export type { InputInfo } from "@narration/types/InputInfo";
+export type { default as LabelRunModeType } from "@narration/types/LabelRunModeType";
+export type { default as LabelSteps } from "@narration/types/LabelSteps";
+export type {
+    StepLabelPropsType,
+    StepLabelResultType,
+    StepLabelType,
+} from "@narration/types/StepLabelType";
 export { narration };
 
 const narration: NarrationManagerInterface = new NarrationManager();
